d4ea4ace988bd0fac2fa9e7d3720423f05511193
DESKTOP-1P9MEJT\rober <robertolopez612@gmail.com>
2024-09-24 18:42:11 +0200

    Added Spring Boot project from Spring Initializer.

---
+++ src/main/java/com/streetart_explorer_auth_server/streetart_explorer_auth_server/StreetartExplorerAuthServerApplication.java
@@ -0,0 +1,13 @@
+package com.streetart_explorer_auth_server.streetart_explorer_auth_server;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StreetartExplorerAuthServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StreetartExplorerAuthServerApplication.class, args);
+	}
+
+}
